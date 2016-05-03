b47f164b1e83826e811fc47c73467673d89d9b92
Sanjay Govind <sanjay.govind9@gmail.com>
2016-05-03 17:02:11 +1200

    Push basic line following code

--- input.cpp
+++ input.cpp
@@ -8,12 +8,27 @@ extern "C" int read_analog(int ch_adc);
 extern "C" char get_pixel(int row,int col,int color);
 
 int main (){
-      // This sets up the RPi hardware and ensures
-      // everything is working correctly
-      init();
-      while (1) {
-          int w = get_pixel(128,160,3);
-          printf("%d\n" ,w);
-      }
-return 0;
+    // This sets up the RPi hardware and ensures
+    // everything is working correctly
+    init();
+    while (1) {
+        int sum = 0;
+        for(int i=-160; i<160; i+=10){
+            int w = get_pixel(128,i+160,3);
+            if (w > 100)
+                sum = sum + i*w;
+        }
+        printf("%d\n" ,sum);
+        if (sum > 0) {
+            set_motor(1,127);
+            set_motor(2,-127);
+        } else if (sum == 0) {
+            set_motor(1,127);
+            set_motor(2,127);
+        } else {
+            set_motor(1,-127);
+            set_motor(2,127);
+        }
+    }
+    return 0;
 }
