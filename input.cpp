f8af2d483056953f42256823f2a0cf5b368d46ed
Sanjay Govind <sanjay.govind9@gmail.com>
2016-05-18 21:27:19 +1200

    commit

--- input.cpp
+++ input.cpp
@@ -6,45 +6,49 @@ extern "C" int set_motor(int motor,int speed);
 extern "C" int Sleep( int sec , int usec );
 extern "C" int read_analog(int ch_adc);
 extern "C" char get_pixel(int row,int col,int color);
-
+static int BASE_SPEED = 22;
+static int MAX_SPEED = 255;
 int main (){
     // This sets up the RPi hardware and ensures
     // everything is working correctly
     init();
-    float kp = 0.5;
-    float ki = 0.5;
-    float kd = 1;
-    int previous_error = 0;
-    int total_error = 0;
-    clock_t begin, end;
+    float kp = 0.2;
+    float ki = 0;
+    float kd = 0;
+    float previous_error = 0;
+    float total_error = 0;
+    float current_error;
+    float proportional_signal;
+    float derivative_signal;
+    float integral_signal;
     while (1) {
-        double time_spent;
-
-        begin = clock();
-
-        int current_error = 0;
-        int proportional_signal;
-        int derivative_signal;
-        int integral_signal;
+        current_error = 0;
         for (int i=0; i<320; i++){
             int w = get_pixel(i, 120, 3);
             if (w > 127) {
-                int error = (i - 160) * w;
+                int error = (i - 160);
                 current_error = current_error + error;
             }
         }
+        current_error /= 20;
         proportional_signal = current_error*kp;
-        //derivative_signal = ((current_error-previous_error))*kd;
-        //integral_signal = ((total_error+current_error))*ki;
+        derivative_signal = ((current_error-previous_error))*kd;
+        integral_signal = ((total_error+current_error))*ki;
         total_error = total_error+current_error;
-            previous_error = current_error;
-        printf("Proportional signal is: %d", proportional_signal );
-        //printf("Integral signal is: %d", integral_signal );
-        //printf("Derivative signal is: %d", derivative_signal );
-        int pid = proportional_signal/*+integral_signal+derivative_signal*/;
-        pid /= (160*1*kp);
-        set_motor(1, -pid);
-        set_motor(2, -pid);
+        previous_error = current_error;
+        printf("Proportional signal is: %f\n", proportional_signal );
+        printf("Integral signal is: %f\n", integral_signal );
+        printf("Derivative signal is: %f\n", derivative_signal );
+        float pid = (proportional_signal+integral_signal+derivative_signal);
+        printf("Signal is: %f\n",pid);
+        int lMSpeed = BASE_SPEED+int(pid);
+        int rMSpeed = BASE_SPEED-int(pid);
+        if (rMSpeed > MAX_SPEED) rMSpeed = MAX_SPEED;
+        if (lMSpeed > MAX_SPEED) lMSpeed = MAX_SPEED;
+        if (rMSpeed < -MAX_SPEED) rMSpeed = -MAX_SPEED;
+        if (lMSpeed < -MAX_SPEED) lMSpeed = -MAX_SPEED;
+        set_motor(1,lMSpeed);
+        set_motor(2,rMSpeed);
     }
     return 0;
 }
