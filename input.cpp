96efba3f97ecc7af2c047d52916b9491d67f3c1a
Lenton <lentonneale@gmail.com>
2016-05-23 22:24:15 +0000

    Test

--- input.cpp
+++ input.cpp
@@ -10,11 +10,11 @@ extern "C" int Sleep( int sec , int usec );
 extern "C" int read_analog(int ch_adc);
 extern "C" unsigned char get_pixel(int row,int col,int color);
 extern "C" int take_picture();
-static float kp = 0.5;
+static float kp = 0.35;
 static float ki = 0;
 static float kd = 0;
 static int WHITE = 127;
-static int BASE_SPEED = 40;
+static int BASE_SPEED = 45;
 void signal_callback_handler(int signum)
 {
     //We caught sig 2 (ctrl+c)
@@ -36,6 +36,8 @@ int main (){
     double proportional_signal;
     double integral_signal;
     double derivative_signal;
+    float counter =0;
+	bool first = false;
     //TODO Uncomment this and test it
     /*connect_to_server((char *) "130.195.6.196", 1024);
     //sends a message to the connected server
@@ -45,21 +47,25 @@ int main (){
     receive_from_server(message);
     send_to_server(message);*/
     bool outofQuadThree = true;
-    while (outofQuadThree) {
+    while (1) {
         take_picture();
         current_error = 0;
-        outofQuadThree = true;
+counter = 0;
+        outofQuadThree = false;
         for(int i=0; i<320; i++){
             //The idea with this, is if we come up to a straight line, we switch to maze solving mode
             //So if a pixel is not white, we havent hit the line yet
             if(get_pixel(i,120,3)>WHITE){
                 current_error += (i-160);
-            } else {
-                outofQuadThree = false;
-            }
+                outofQuadThree = true;
+		counter++; 
+	} 
         }
-        if (outofQuadThree) break;
-        current_error/=160;
+	current_error/=160;
+
+        if (counter > 280 && !first)  break;
+	if (counter > 280) first = false;
+//        current_error/=160;
         total_error = total_error+current_error;
         integral_signal = total_error*ki;
         derivative_signal = (current_error-previous_error/0.1)*kd;
@@ -67,10 +73,13 @@ int main (){
         int pid =- int(proportional_signal+integral_signal+derivative_signal);
         set_motor(1, (BASE_SPEED +  pid));
         set_motor(2, (BASE_SPEED - pid));
-        printf("pid:%d\n",pid);
+        printf("pid:%f\n",current_error);
     }
+    set_motor(1,0);
+	set_motor(2,0);
+    return 0;
     printf("%s\n","Switching to Quadrant 3");
-    while (!outofQuadThree) {
+    while (outofQuadThree) {
         outofQuadThree = false;
         for(int i=0; i<320; i++){
             //If the grayness is > white, add to error
@@ -112,6 +121,7 @@ int main (){
         set_motor(1,BASE_SPEED);
         set_motor(2,BASE_SPEED);
     }
+
     printf("%s\n","Switching to maze mode");
 
     double unc = 8;
