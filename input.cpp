93468aea664abf2f7134e127bce9ab41d30bc552
Sanjay Govind <sanjay.govind9@gmail.com>
2016-05-23 17:56:40 +1200

    Code makes it to quad 3

--- input.cpp
+++ input.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
-
+#include <cstdlib>
+#include <signal.h>
 extern "C" int init();
 extern "C" int connect_to_server( char server_addr[15],int port);
 extern "C" int send_to_server(char message[24]);
@@ -9,14 +10,27 @@ extern "C" int Sleep( int sec , int usec );
 extern "C" int read_analog(int ch_adc);
 extern "C" unsigned char get_pixel(int row,int col,int color);
 extern "C" int take_picture();
-static float kp = 0.7;
+static float kp = 0.5;
 static float ki = 0;
-static float kd = 0.2;
+static float kd = 0;
 static int WHITE = 127;
-static int BASE_SPEED = 52;
+static int BASE_SPEED = 40;
 typedef enum {UP, LEFT, RIGHT,DOWN} direction;
+void
+signal_callback_handler(int signum)
+{
+   printf("Caught signal %d\n",signum);
+   // Cleanup and close up stuff here
+	set_motor(1,0);
+	set_motor(2,0);
+   // Terminate program
+   exit(signum);
+}
+
 int main (){
-    init();
+	init();
+	signal(2, signal_callback_handler);
+   
     float current_error = 0;
     float ahead_current_error = 0;
     float left_error = 0;
@@ -37,7 +51,7 @@ int main (){
     send_to_server(message);*/
     //read_analog(0) = left, read_analog(1) = front, read_analog(2) = right, read_analog(3) = back;
     //TODO: Put something in this loop to break out. going to need to test sensor values to work this one out
-    while (/*read_analog(1) < 10 && read_analog(3) < 10*/ get_pixel(160, 120, 3)<WHITE && get_pixel(160, 120, 0)>WHITE) {
+    while (1) {
         //connects to server with the ip address 192.168.1.2
 
         take_picture();
@@ -58,28 +72,28 @@ int main (){
                ahead_current_error += (i-160);
            }
         }
-        printf("%f\n",ahead_current_error/120*kp);
+        printf("%f\n",current_error);
+	printf("%f\n",ahead_current_error);
         //Implement a favour straight, left then right algo here.
         //This may require reading more pixels than just whats in the center, but thats okay
-        if (ahead_current_error/120*kp < 2 && ahead_current_error/120*kp > -2) {
-            if (current_error/120*kp > 2 || current_error/120*kp < -2)
-            current_error = 0;
-        } else {
             //Theres a line to the left, and a line to the right. Lets go left
-            if (left_error/120*kp > 10 && right_error > left_error) {
-                printf("%f\n",left_error/120*kp);
-                current_error -=left_error;
+            if (left_error/160*kp > 10 && right_error > left_error) {
+                printf("%f\n",left_error/160*kp);
+                set_motor(1,10);
+		set_motor(2,-80);
+		Sleep(1,000000);
             }
-        }
+        
 
         current_error/=(160);
         total_error = total_error+current_error;
         integral_signal = total_error*ki;
         derivative_signal = (current_error-previous_error/0.1)*kd;
         proportional_signal = current_error*kp;
-        int pid = int(proportional_signal+integral_signal+derivative_signal);
-        set_motor(1, -(BASE_SPEED +  pid));
-        set_motor(2, -(BASE_SPEED - pid));
+        int pid =- int(proportional_signal+integral_signal+derivative_signal);
+        set_motor(1, (BASE_SPEED +  pid));
+        set_motor(2, (BASE_SPEED - pid));
+	printf("pid:%d\n",pid);
     }
     printf("%s\n","Switching to maze mode");
     //This probably wont work, but is a basis of something similar to what we want to do.
@@ -101,32 +115,32 @@ int main (){
                 if(right - left > 1 && right - left < unc){
                     //motor1 is left motor
                     //motor2 is right motor
-                    setMotor(1, BASE_SPEED+5);
-                    setMotor(2, BASE_SPEED);
+                    set_motor(1, BASE_SPEED+5);
+                    set_motor(2, BASE_SPEED);
                     //move trajectory slightly to the right
                 } else if (left - right > 1){
-                    setMotor(1, BASE_SPEED);
-                    setMotor(2, BASE_SPEED+5);
+                    set_motor(1, BASE_SPEED);
+                    set_motor(2, BASE_SPEED+5);
                     //move trajectory slightly left
                 } else {
-                    setMotor(1, BASE_SPEED);
-                    setMotor(2, BASE_SPEED);
+                    set_motor(1, BASE_SPEED);
+                    set_motor(2, BASE_SPEED);
                     //move robot forward
                 }
             } else {
                 if(0 < left && left < unc){
                     //turn around
-                    setMotor(1, -(BASE_SPEED));
-                    setMotor(2, BASE_SPEED);
+                    set_motor(1, -(BASE_SPEED));
+                    set_motor(2, BASE_SPEED);
                 } else {
-                    setMotor(1, BASE_SPEED/3);
-                    setMotor(2, BASE_SPEED);
+                    set_motor(1, BASE_SPEED/3);
+                    set_motor(2, BASE_SPEED);
                     //turn left
                 }
             }
         } else {
-            setMotor(1, BASE_SPEED);
-            setMotor(2, BASE_SPEED/3);
+            set_motor(1, BASE_SPEED);
+            set_motor(2, BASE_SPEED/3);
             //turn right;
         }
     }
