6fe88f223e01281cccf9f272ca87d889cb573484
Sanjay Govind <sanjay.govind9@gmail.com>
2016-05-31 19:14:16 +1200

    I keep using java syntax

--- input.cpp
+++ input.cpp
@@ -75,7 +75,7 @@ int main (){
         }
         bool red = false;
         for(int i=0; i<320; i++){
-            if ((double)get_pixel(i,160,0)/(double)get_pixel(i,160,3) > 1f) {
+            if ((double)get_pixel(i,160,0)/(double)get_pixel(i,160,3) > 1) {
                 red = true;
             }
         }
